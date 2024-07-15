772ccc5b839b22154011d415fa50d6417d0d840b
Blissmal <malutibethuel@gmail.com>
2024-07-15 19:38:26 +0300

    updated

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_API_KEY,
@@ -16,3 +17,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth()
 export const db = getFirestore()
+export const storage = getStorage()
