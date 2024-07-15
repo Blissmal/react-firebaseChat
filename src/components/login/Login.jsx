2349aaba1b0c151eb5c5225088f24887979f4706
Blissmal <malutibethuel@gmail.com>
2024-07-15 20:24:14 +0300

    updated firebase

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react'
 import "./login.css"
 import { toast } from 'react-toastify'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
-import { auth } from "../../lib/firebase"
+import { auth, db } from "../../lib/firebase"
+import { doc, setDoc } from 'firebase/firestore'
 
 
 const Login = () => {
@@ -31,6 +32,18 @@ const Login = () => {
 
         try {
             const res = await createUserWithEmailAndPassword(auth, email, password)
+            
+            await setDoc(doc(db, "users", res.user.uid), {
+                username,
+                email,
+                id: res.user.uid,
+                blocked: [],
+            });
+            await setDoc(doc(db, "userchats", res.user.uid), {
+                chats: []
+            });
+
+            toast.success("Account created successfully")
         } catch (err) {
             console.log(err)
             toast.error(err.message)
