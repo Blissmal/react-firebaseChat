2916d347695a56d8cbc2872d5bf0564d2c28ea14
Blissmal <malutibethuel@gmail.com>
2024-07-15 19:27:26 +0300

    initialized firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: "react-chat-179d8.firebaseapp.com",
+    projectId: "react-chat-179d8",
+    storageBucket: "react-chat-179d8.appspot.com",
+    messagingSenderId: "1092168258638",
+    appId: "1:1092168258638:web:4c6366370065dea30b5f1f"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth()
+export const db = getFirestore()
