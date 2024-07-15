9754a09b34958a2a58edf8eecd550422f2b7d0c8
Blissmal <malutibethuel@gmail.com>
2024-07-15 20:06:15 +0300

    firebase authentication signp working fine

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import "./login.css"
 import { toast } from 'react-toastify'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth } from "../../lib/firebase"
+
 
 const Login = () => {
 
@@ -19,9 +22,23 @@ const Login = () => {
         }
     }
 
+    const handleRegister = async (e) => {
+        e.preventDefault()
+        const formData = new FormData(e.target)
+
+        const { username, email, password } = Object.fromEntries(formData);
+
+
+        try {
+            const res = await createUserWithEmailAndPassword(auth, email, password)
+        } catch (err) {
+            console.log(err)
+            toast.error(err.message)
+        }
+    }
     const handleLogin = e => {
         e.preventDefault()
-        
+
     }
 
     return (
@@ -37,7 +54,7 @@ const Login = () => {
             <div className="separator"></div>
             <div className="item">
                 <h2>Create an Account</h2>
-                <form action="">
+                <form onSubmit={handleRegister}>
                     <label htmlFor="file">
                         <img src={avatar.url || "./avatar.png"} alt="" />
                         Upload an image</label>
