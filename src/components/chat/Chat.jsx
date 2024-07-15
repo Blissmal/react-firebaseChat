d186ca036c132686da15469a1bc70dd095d0c583
Blissmal <malutibethuel@gmail.com>
2024-07-15 11:42:54 +0300

    updated

---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./chat.css"
+
+const Chat = () => {
+  return (
+    <div className='chat'>
+      
+    </div>
+  )
+}
+
+export default Chat
---
+++ src/components/detail/Detail.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./detail.css"
+
+const Detail = () => {
+  return (
+    <div className='detail'>
+      
+    </div>
+  )
+}
+
+export default Detail
