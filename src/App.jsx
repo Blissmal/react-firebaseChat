7a96bb28fae0b396222d5cafb2b1fc2603904ba4
Blissmal <malutibethuel@gmail.com>
2024-07-15 16:05:40 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import Chat from "./components/chat/Chat"
 import Detail from "./components/detail/Detail"
 import List from "./components/list/List"
+import Login from "./components/login/Login"
+import Notification from "./components/notification/Notification"
 
 const App = () => {
+
+  const user = false
+
   return (
     <div className='container'>
-      <List />
-      <Chat />
-      <Detail />
-    </div>
+      {
+        user ? (
+          <>
+            <List />
+            <Chat />
+            <Detail />
+          </>
+        ) : (
+          <Login />
+          )
+}
+  <Notification />
+    </div >
   )
 }
 
