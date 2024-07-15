b190e15bc8dc0c169e9628756849e8f07ba409c2
Blissmal <malutibethuel@gmail.com>
2024-07-15 16:17:28 +0300

    added file addUser

--- src/components/addUser/addUser.jsx
+++ src/components/addUser/addUser.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import "./addUser.css"
 
 const addUser = () => {
   return (
-    <div>
-      
+    <div className='addUser'>
+      <form action="">
+        <input type="text" placeholder='Username' name='username'/>
+        <button>Search</button>
+      </form>
+      <div className="user">
+        <div className="detail">
+            <img src="./avatar.png" alt="" />
+            <span>Jane Doe</span>
+        </div>
+        <button>Add User</button>
+      </div>
     </div>
   )
 }
