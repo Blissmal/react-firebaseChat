513aed9866f44ebd56b37a2e69a76357c5e70d38
Blissmal <malutibethuel@gmail.com>
2024-07-15 11:50:20 +0300

    updated

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -4,7 +4,22 @@ import "./chat.css"
 const Chat = () => {
   return (
     <div className='chat'>
-      
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="botton"></div>
     </div>
   )
 }
