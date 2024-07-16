1106b1df8645f7c43b55a401034cae87c81f1d41
Blissmal <malutibethuel@gmail.com>
2024-07-16 17:50:22 +0300

    functional chat

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react'
 import "./chat.css"
 import EmojiPicker from "emoji-picker-react"
-import { doc, onSnapshot } from 'firebase/firestore'
+import { arrayUnion, doc, getDoc, onSnapshot, updateDoc } from 'firebase/firestore'
 import { db } from '../../lib/firebase'
 import { useChatStore } from '../../lib/chatStore'
+import { useUserStore } from '../../lib/userStore'
 
 const Chat = () => {
 
@@ -11,7 +12,8 @@ const Chat = () => {
   const [open, setOpen] = useState(false)
   const [text, setText] = useState("")
 
-  const { chatId } = useChatStore()
+  const { chatId, user } = useChatStore()
+  const { currentUser } = useUserStore()
 
   const endRef = useRef(null);
 
@@ -39,61 +41,100 @@ const Chat = () => {
     setOpen(false)
   }
 
-  return (
-    <div className='chat'>
-      <div className="top">
-        <div className="user">
-          <img src="./avatar.png" alt="" />
-          <div className="texts">
-            <span>Jane Doe</span>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-        </div>
-        <div className="icons">
-          <img src="./phone.png" alt="" />
-          <img src="./video.png" alt="" />
-          <img src="./info.png" alt="" />
-        </div>
-      </div>
-      <div className="center">
-        {
-          chat?.messages?.map(message => (
+  const handleSend = async () => {
+    if (text === "") return;
 
-            <div className="message own" key={message?.createAt}>
-              <div className="texts">
-                {
-                  message.img && <img
-                    src={message.img}
-                    alt=''
-                  ></img>
-                }
-                <p>
-                  {message.text}
-                </p>
-                {/* <span>1 min ago</span> */}
-              </div>
-            </div>
-          ))
+    try {
+
+      await updateDoc(doc(db, "chats", chatId), {
+        messages: arrayUnion({
+          senderId: currentUser.id,
+          text,
+          createdAt: new Date(),
+        }),
+      });
+
+      const userIds = [currentUser.id, user.id]
+
+      userIds.forEach(async (id) => {
+
+
+        const userChatsRef = doc(db, "userchats", id)
+        const userChatsSnapshot = await getDoc(userChatsRef)
+
+        if (userChatsSnapshot.exists()) {
+          const userChatsData = userChatsSnapshot.data()
+
+          const chatIndex = userChatsData.chats.findIndex(c => c.chatId === chatId)
+
+          userChatsData.chats[chatIndex].lastMessage = text
+          userChatsData.chats[chatIndex].isSeen = id === currentUser.id ? true: false
+          userChatsData.chats[chatIndex].updatedAt = Date.now()
+
+          await updateDoc(userChatsRef, {
+            chats: userChatsData.chats,
+
+          });
         }
-        <div ref={endRef}></div>
+      });
+
+    } catch (err) {
+    console.log(err)
+  }
+}
+
+return (
+  <div className='chat'>
+    <div className="top">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
       </div>
-      <div className="bottom">
-        <div className="icons">
-          <img src="./img.png" alt="" />
-          <img src="./camera.png" alt="" />
-          <img src="./mic.png" alt="" />
-        </div>
-        <input type="text" name="" id="" placeholder='Type a message...' onChange={e => setText(e.target.value)} value={text} />
-        <div className="emoji">
-          <img src="./emoji.png" alt="" onClick={() => setOpen(prev => !prev)} />
-          <div className="picker">
-            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
-          </div>
-        </div>
-        <button className='sendButton'>Send</button>
+      <div className="icons">
+        <img src="./phone.png" alt="" />
+        <img src="./video.png" alt="" />
+        <img src="./info.png" alt="" />
       </div>
     </div>
-  )
+    <div className="center">
+      {
+        chat?.messages?.map((message) => (
+
+          <div className="message own" key={message?.createAt}>
+            <div className="texts">
+              {
+                message.img && <img src={message.img} alt='' ></img>
+              }
+              <p>
+                {message.text}
+              </p>
+              {/* <span>1 min ago</span> */}
+            </div>
+          </div>
+        ))
+      }
+      <div ref={endRef}></div>
+    </div>
+    <div className="bottom">
+      <div className="icons">
+        <img src="./img.png" alt="" />
+        <img src="./camera.png" alt="" />
+        <img src="./mic.png" alt="" />
+      </div>
+      <input type="text" name="" id="" placeholder='Type a message...' onChange={e => setText(e.target.value)} value={text} />
+      <div className="emoji">
+        <img src="./emoji.png" alt="" onClick={() => setOpen(prev => !prev)} />
+        <div className="picker">
+          <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+        </div>
+      </div>
+      <button className='sendButton' onClick={handleSend}>Send</button>
+    </div>
+  </div>
+)
 }
 
 export default Chat
