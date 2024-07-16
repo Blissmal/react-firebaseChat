1617310470a758689ed4dabf72551b839fff9661
Blissmal <malutibethuel@gmail.com>
2024-07-16 21:01:30 +0300

    updated

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -5,12 +5,17 @@ import { arrayUnion, doc, getDoc, onSnapshot, updateDoc } from 'firebase/firesto
 import { db } from '../../lib/firebase'
 import { useChatStore } from '../../lib/chatStore'
 import { useUserStore } from '../../lib/userStore'
+import upload from '../../lib/upload'
 
 const Chat = () => {
 
   const [chat, setChat] = useState()
   const [open, setOpen] = useState(false)
   const [text, setText] = useState("")
+  const [img, setImg] = useState({
+    file: null,
+    url: "",
+  })
 
   const { chatId, user } = useChatStore()
   const { currentUser } = useUserStore()
@@ -41,16 +46,33 @@ const Chat = () => {
     setOpen(false)
   }
 
+  const handleImg = e => {
+    if (e.target.files[0]) {
+
+      setImg({
+        file: e.target.files[0],
+        url: URL.createObjectURL(e.target.files[0])
+      })
+    }
+  }
+
   const handleSend = async () => {
     if (text === "") return;
 
+    let imgUrl = null
+
     try {
+
+      if (img.file) {
+        imgUrl = await upload(img.file)
+      }
 
       await updateDoc(doc(db, "chats", chatId), {
         messages: arrayUnion({
           senderId: currentUser.id,
           text,
           createdAt: new Date(),
+          ...(imgUrl && { img: imgUrl })
         }),
       });
 
@@ -68,7 +90,7 @@ const Chat = () => {
           const chatIndex = userChatsData.chats.findIndex(c => c.chatId === chatId)
 
           userChatsData.chats[chatIndex].lastMessage = text
-          userChatsData.chats[chatIndex].isSeen = id === currentUser.id ? true: false
+          userChatsData.chats[chatIndex].isSeen = id === currentUser.id ? true : false
           userChatsData.chats[chatIndex].updatedAt = Date.now()
 
           await updateDoc(userChatsRef, {
@@ -79,62 +101,73 @@ const Chat = () => {
       });
 
     } catch (err) {
-    console.log(err)
+      console.log(err)
+    }
+
+    setImg({
+      file: null,
+      url: ""
+    })
+
+    setText("")
+
   }
-}
 
-return (
-  <div className='chat'>
-    <div className="top">
-      <div className="user">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Jane Doe</span>
-          <p>Lorem ipsum dolor sit amet.</p>
-        </div>
-      </div>
-      <div className="icons">
-        <img src="./phone.png" alt="" />
-        <img src="./video.png" alt="" />
-        <img src="./info.png" alt="" />
-      </div>
-    </div>
-    <div className="center">
-      {
-        chat?.messages?.map((message) => (
-
-          <div className="message own" key={message?.createAt}>
-            <div className="texts">
-              {
-                message.img && <img src={message.img} alt='' ></img>
-              }
-              <p>
-                {message.text}
-              </p>
-              {/* <span>1 min ago</span> */}
-            </div>
+  return (
+    <div className='chat'>
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Lorem ipsum dolor sit amet.</p>
           </div>
-        ))
-      }
-      <div ref={endRef}></div>
-    </div>
-    <div className="bottom">
-      <div className="icons">
-        <img src="./img.png" alt="" />
-        <img src="./camera.png" alt="" />
-        <img src="./mic.png" alt="" />
-      </div>
-      <input type="text" name="" id="" placeholder='Type a message...' onChange={e => setText(e.target.value)} value={text} />
-      <div className="emoji">
-        <img src="./emoji.png" alt="" onClick={() => setOpen(prev => !prev)} />
-        <div className="picker">
-          <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
         </div>
       </div>
-      <button className='sendButton' onClick={handleSend}>Send</button>
+      <div className="center">
+        {
+          chat?.messages?.map((message) => (
+
+            <div className="message own" key={message?.createAt}>
+              <div className="texts">
+                {
+                  message.img && <img src={message.img} alt='' ></img>
+                }
+                <p>
+                  {message.text}
+                </p>
+                {/* <span>1 min ago</span> */}
+              </div>
+            </div>
+          ))
+        }
+        <div ref={endRef}></div>
+      </div>
+      <div className="bottom">
+        <div className="icons">
+          <label htmlFor="file">
+            <img src="./img.png" alt="" />
+          </label>
+          <input type="file" id='file' style={{ display: "none" }} onChange={handleImg} />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input type="text" name="" id="" placeholder='Type a message...' onChange={e => setText(e.target.value)} value={text} />
+        <div className="emoji">
+          <img src="./emoji.png" alt="" onClick={() => setOpen(prev => !prev)} />
+          <div className="picker">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button className='sendButton' onClick={handleSend}>Send</button>
+      </div>
     </div>
-  </div>
-)
+  )
 }
 
 export default Chat
