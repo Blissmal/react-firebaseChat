bee7417ff5143ebd6961453e12d04605eca7677f
Blissmal <malutibethuel@gmail.com>
2025-04-05 23:52:09 +0300

    update

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import "./chatList.css"
 import AddUser from './addUser/addUser'
 import { useUserStore } from "../../../lib/userStore"
